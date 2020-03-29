ReloadedOS/vendor_reloaded

vendor: added APN config files to prebuilt
Change-Id: Id6d84a1698b7c4e6e58170c93cd6a61e49bda966

configs/common.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     vendor/wave/prebuilt/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     vendor/wave/prebuilt/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Build Snapdragon apps
 PRODUCT_PACKAGES += \
     SnapdragonGallery \

yashsonawane/platform_vendor_lucid

configs/common.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 # Common overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/lucid/overlay
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/lucid/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Branding stuffs
 LUCID_VERSION := 1.0
 LUCID_VERSION_CODE := Queen-Cake