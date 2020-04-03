namdeptrai2003/android_vendor_dnd

Adapt to 6.0.1_r52 backup whitelist
As of f/b 9b8c6d2df35455ce9e67907edded1e4a2ecb9e28
 we need to whitelist who can be backup services.

Change-Id: I52a004924e6d2651c5ecdec51a8c5a80bdd54a38

config/common.mk
@@ -84,6 +84,10 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/cm/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh

yashsonawane/platform_vendor_lucid

Signed-off-by: devabhixda <devabhixda@gmail.com>
Conflicts:
	config/common.mk

configs/common.mk
@@ -46,6 +46,10 @@ DEVICE_PACKAGE_OVERLAYS += vendor/lucid/overlay
 PRODUCT_COPY_FILES += \
     vendor/lucid/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/lucid/configs/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
 # Branding stuffs
 LUCID_VERSION := 1.0
 LUCID_VERSION_CODE := Queen-Cake